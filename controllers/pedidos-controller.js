6be6aacf9f8952313a39ea6419130cd263037d50
AFONSOEIRAS <afonsoeiras26@gmail.com>
2021-12-10 22:03:29 -0300

    Protegendo as rotas com JWT

---
+++ controllers/pedidos-controller.js
@@ -0,0 +1,3 @@
+exports.getPedidos = (req, res, next) => {
+
+}
--- routes/produtos.js
+++ routes/produtos.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("../mysql").pool;
 const multer = require("multer"); //imagens
+const login = require("../middleware/login");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -61,8 +62,8 @@ router.get("/",(req, res, next) =>{
 });
 
 //INSERE UM PRODUTO
-router.post("/", upload.single("produto_imagem") ,(req, res, next) => {
-
+router.post("/", login.obrigatorio, upload.single("produto_imagem"), (req, res, next) => {
+    console.log(req.usuario);
     mysql.getConnection((error, conn) =>{
         if(error) {return res.status(500).send({error: error})}
         conn.query(
@@ -134,7 +135,7 @@ router.get("/:id_produto",(req, res, next) => {
 });
 
 //ALTERA UM PRODUTO
-router.patch("/", (req, res, next) =>{
+router.patch("/", login.obrigatorio, (req, res, next) =>{
     mysql.getConnection((error, conn) =>{
         if(error) {return res.status(500).send({error: error})}
         conn.query(
@@ -170,7 +171,7 @@ router.patch("/", (req, res, next) =>{
 });
 
 //EXCLUI UM PRODUTO
-router.delete("/", (req, res, next) =>{
+router.delete("/", login.obrigatorio, (req, res, next) =>{
     mysql.getConnection((error, conn) =>{
         if(error) {return res.status(500).send({error: error})}
         conn.query(
